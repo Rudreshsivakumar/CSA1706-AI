4d09523c095258519dbb09cf7532ef3341e1446f
Rudreshsivakumar <142885242+Rudreshsivakumar@users.noreply.github.com>
2023-12-01 09:16:14 +0530

    Create 28.pl

---
+++ 28.pl
@@ -0,0 +1,21 @@
+% Facts about symptoms and diseases
+symptom(john, fever).
+symptom(john, cough).
+symptom(john, fatigue).
+
+symptom(susan, headache).
+symptom(susan, sore_throat).
+symptom(susan, fatigue).
+
+disease(fever, flu).
+disease(cough, flu).
+disease(fatigue, flu).
+
+disease(headache, cold).
+disease(sore_throat, cold).
+disease(fatigue, cold).
+
+% Rule to determine if a person has a particular disease based on symptoms
+has_disease(Person, Disease) :-
+    symptom(Person, Symptom),
+    disease(Symptom, Disease).
